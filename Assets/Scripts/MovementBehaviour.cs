ac3560ec09960682e95c052fef37f45851cc3561
Ádám Knapecz <knapecz.adam@gmail.com>
2024-09-24 19:33:29 +0200

    Add virtual camera, follow the player Rotate the player to face the direction of the movement

--- Assets/Scripts/MovementBehaviour.cs
+++ Assets/Scripts/MovementBehaviour.cs
@@ -8,6 +8,11 @@ public class MovementBehaviour : MonoBehaviour
     [SerializeField]
     private float _movementSpeed = 1.0f;
     
+    [SerializeField]
+    private float _rotationSpeed = 10.0f;
+    
+    private Rigidbody _rigidbody;
+    
     private Vector3 _desiredMovementDirection = Vector3.zero;
     public Vector3 DesiredMovementDirection 
     { 
@@ -15,15 +20,41 @@ public class MovementBehaviour : MonoBehaviour
         set => _desiredMovementDirection = value; 
     }
 
-    private void Update()
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
     {
         HandleMovement();
+        HandleRotation();
     }
     
     private void HandleMovement()
     {
+        if (_rigidbody is null)
+        {
+            Debug.LogError("Rigidbody is not set in the MovementBehaviour component.");
+            return;
+        }
+        
         Vector3 movement = _desiredMovementDirection.normalized;
         movement *= _movementSpeed * Time.deltaTime;
-        transform.position += movement;
+        // transform.position += movement;
+        _rigidbody.MovePosition(transform.position + movement);
+    }
+    
+    private void HandleRotation()
+    {
+        if (_desiredMovementDirection != Vector3.zero)
+        {
+            // Rotates the character to face the direction of movement
+            // transform.rotation = Quaternion.LookRotation(_desiredMovementDirection);
+            
+            // Smoothly rotates the character to face the direction of movement.
+            Quaternion targetRotation = Quaternion.LookRotation(_desiredMovementDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
+        }
     }
 }
